f5e53b4f85d9cc422f52035fa675d3d85511e4fb
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-03 15:40:13 +0800

    UsersController

---
+++ YeeOffice/YeeOffice.UserCenter.Service/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YeeOffice.UserCenter.BusnissLogic;
+using YeeOffice.UserCenter.Entities;
+
+namespace Ak.Projects.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        public UserBusnissLogic BusnissLogic { get; set; }
+
+        public UsersController(UserBusnissLogic busnissLogic)
+        {
+            BusnissLogic = busnissLogic;
+        }
+
+        //[HttpPost]
+        [HttpGet]
+        public string Index()
+        {
+            var user = new UserEntity();
+            user.Account = "bruce.chen";
+            user.Password = "123";
+            user.DisplayName = "Bruce Chen";
+            BusnissLogic.Save(user);
+            return "value";
+        }
+    }
+}
