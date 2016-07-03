ab06c14b70ab9efee30e78686489669eb7430bdb
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-03 11:15:37 +0800

    DataAccess

---
+++ YeeOffice/YeeOffice.Common/DataAccess/Interface/IDatabase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Data;
+
+namespace Ak.Projects.Common.DataAccess
+{
+    public interface IDatabase : IDbConnection, IDisposable
+    {
+    }
+}
---
+++ YeeOffice/YeeOffice.Common/DataAccess/Interface/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+
+namespace Ak.Projects.Common.DataAccess
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IDatabase Database { get; set; }
+        IDbTransaction Transaction { get; set; }
+        void SaveChanges();
+    }
+}
