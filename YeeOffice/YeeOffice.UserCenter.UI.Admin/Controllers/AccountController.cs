9a1716ace12c20f74841dc1f6742cc6c8b086866
Bruce <Bruce@DESKTOP-056M100>
2016-07-03 21:37:27 +0800

    login

--- YeeOffice/YeeOffice.UserCenter.UI.Admin/Controllers/AccountController.cs
+++ YeeOffice/YeeOffice.UserCenter.UI.Admin/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using YeeOffice.UserCenter.BusnissLogic;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,13 @@ namespace YeeOffice.UserCenter.UI.Admin.Controllers
 {
     public class AccountController : Controller
     {
+        public UserBusnissLogic BusnissLogic { get; set; }
+
+        public AccountController(UserBusnissLogic busnissLogic)
+        {
+            BusnissLogic = busnissLogic;
+        }
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Login()
@@ -18,8 +26,11 @@ namespace YeeOffice.UserCenter.UI.Admin.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(string username,string password)
+        public IActionResult Login(string username, string password)
         {
+            var logined = BusnissLogic.Login(username, password);
+
+            if (logined) return Redirect("/Home/Index/");
             return View();
         }
     }
