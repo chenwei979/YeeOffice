78a24198a2b6052a865ef1c8070de5fd56727a60
Bruce <Bruce@DESKTOP-056M100>
2016-07-03 17:09:09 +0800

    js

--- YeeOffice/YeeOffice.UserCenter.UI.Admin/wwwroot/_references.js
+++ YeeOffice/YeeOffice.UserCenter.UI.Admin/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="js/sb-admin-2.js" />
+/// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
