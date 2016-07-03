10c449522950045eea09506f96462b81a4bfa5e7
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-03 10:41:44 +0800

    YeeOffice.UserCenter.BusnissLogic

---
+++ YeeOffice/YeeOffice.UserCenter.BusnissLogic/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YeeOffice.UserCenter.BusnissLogic")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50559925-7b5a-4589-a8e4-e4774d73dc0d")]
