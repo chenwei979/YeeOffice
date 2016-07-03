61dccb38dcd5fc2601639251f5efd5ff236b596e
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-03 15:31:45 +0800

    UserBusnissLogic

---
+++ YeeOffice/YeeOffice.UserCenter.BusnissLogic/UserBusnissLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using YeeOffice.UserCenter.DataAccess;
+using YeeOffice.UserCenter.Entities;
+
+namespace YeeOffice.UserCenter.BusnissLogic
+{
+    public class UserBusnissLogic : IDisposable
+    {
+        public UserDataAccess DataAccess { get; set; }
+
+        public UserBusnissLogic(UserDataAccess dataAccess)
+        {
+            DataAccess = dataAccess;
+        }
+
+        public void Save(params UserEntity[] entities)
+        {
+            DataAccess.Save(entities);
+        }
+
+        public void GetItemById(long id)
+        {
+            DataAccess.GetItemById(id);
+        }
+
+        public IList<UserEntity> GetAllItems()
+        {
+            return DataAccess.GetAllItems();
+        }
+
+        public void Dispose()
+        {
+            DataAccess.Dispose();
+        }
+    }
+}
