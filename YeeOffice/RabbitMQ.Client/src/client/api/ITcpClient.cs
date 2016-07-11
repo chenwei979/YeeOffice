a80efb0e55866bdd3932b51c82c0daaaa2ff194d
Bruce Chen <Chenxr@DESKTOP-0U3AGA6>
2016-07-11 17:51:17 +0800

    Add RabbitMQ.Client

---
+++ YeeOffice/RabbitMQ.Client/src/client/api/ITcpClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using System.Net.Sockets;
+
+namespace RabbitMQ.Client
+{
+    /// <summary>
+    /// Wrapper interface for standard TCP-client. Provides socket for socket frame handler class. 
+    /// </summary>
+    /// <remarks>Contains all methods that are currenty in use in rabbitmq client.</remarks>
+    public interface ITcpClient
+    {
+        bool Connected { get; }
+
+        int ReceiveTimeout { get; set; }
+
+        Socket Client { get; }
+
+        Task ConnectAsync(string host, int port);
+
+        NetworkStream GetStream();
+
+        void Close();
+    }
+}
---
+++ YeeOffice/RabbitMQ.Client/src/client/impl/TcpClientAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace RabbitMQ.Client
+{
+
+
+    /// <summary>
+    /// Simple wrapper around TcpClient. 
+    /// </summary>
+    public class TcpClientAdapter : ITcpClient
+    {
+        protected Socket sock;
+
+        public TcpClientAdapter(Socket socket)
+        {
+            if (socket == null)
+                throw new InvalidOperationException("socket must not be null");
+        
+            this.sock = socket;
+        }
+
+        public virtual async Task ConnectAsync(string host, int port)
+        {
+            var adds = await System.Net.Dns.GetHostAddressesAsync(host);
+            var ep = adds.First();
+            #if CORECLR
+            await sock.ConnectAsync(ep, port);
+            #else
+            sock.Connect(ep, port);
+            await System.Threading.Tasks.Task.FromResult (false);
+            #endif
+        }
+
+        public virtual void Close()
+        {
+            sock.Dispose();
+        }
+
+        public virtual NetworkStream GetStream()
+        {
+            return new NetworkStream(sock);
+        }
+
+        public virtual Socket Client
+        {
+            get
+            {
+                return sock;
+            }
+        }
+
+        public virtual bool Connected
+        {
+            get { return sock.Connected; }
+        }
+
+        public virtual int ReceiveTimeout
+        {
+            get
+            {
+                return sock.ReceiveTimeout;
+            }
+            set
+            {
+                sock.ReceiveTimeout = value;
+            }
+        }
+    }
+}
